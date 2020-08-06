de57cbb2cecdf4b86e15da7cb13322ee9ccd1387
crojastriana3 <cristianrojas@unimayor.edu.co>
2020-08-06 17:07:04 -0500

    Inicio y carusel listos

--- js/init.js
+++ js/init.js
@@ -1,19 +1,23 @@
 (function($){
     $(function(){
-  
+      
       $('.sidenav').sidenav();
       $('.parallax').parallax();
+      $('.carousel').carousel();
   
     }); // end of document ready
-        
+    
     $('.carousel.carousel-slider').carousel({
       fullWidth: true
     });
 
     $('.slider').slider();
     $('.tabs').tabs();
+    
 
   })(jQuery); // end of jQuery name space
   $(document).ready(function(){
     $('select').formSelect();
   });
+  
+  
