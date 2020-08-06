b71e091ac1dcb0e793ca15a500d9522d4c794e0d
crojastriana3 <cristianrojas@unimayor.edu.co>
2020-08-06 15:37:07 -0500

    Listo nav

---
+++ js/init.js
@@ -0,0 +1,19 @@
+(function($){
+    $(function(){
+  
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+  
+    }); // end of document ready
+        
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true
+    });
+
+    $('.slider').slider();
+    $('.tabs').tabs();
+
+  })(jQuery); // end of jQuery name space
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
