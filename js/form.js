f908e986ab25778c87bddf01dd81fd62ce7138c4
crojastriana3 <cristianrojas@unimayor.edu.co>
2020-11-16 14:17:38 -0500

    Btn con value

--- js/form.js
+++ js/form.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
   $(".datepicker").datepicker();
 });
+
+// let prod = document
+//   .getElementById("productosN")
+//   .addEventListener("click", (e) => {
+//     // let value = prod.value();
+//     console.log("Ok click");
+//   });
+
+function fnElement(comp) {
+  let value = comp.value;
+  console.log("El valor es: " + value);
+  document.getElementById(
+    "productoSeleccionado"
+  ).innerText = `Elegiste: ${value}.`;
+}
+// si es torta poner un checklist de 6 o 15 porciones
