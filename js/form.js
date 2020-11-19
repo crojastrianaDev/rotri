ec73d62dbf836341c2e56812943225788302c2cc
crojastriana3 <cristianrojas@unimayor.edu.co>
2020-11-19 15:34:19 -0500

    Ok si no es torta a wh

--- js/form.js
+++ js/form.js
@@ -1,24 +1,31 @@
 $(document).ready(function () {
   $(".datepicker").datepicker();
   $("select").formSelect();
+  $(".timepicker").timepicker();
+  $(".tooltipped").tooltip({ delay: 500 });
+  $(".tooltipped").trigger("mouseleave");
+  $(".tooltipped").trigger("needs click");
 });
 
 const porciones = document.getElementById("porciones");
 let cantPorciones = document.getElementById("cantPorciones");
+let boolean = false;
+let value;
 
 function fnElement(comp) {
-  let value = comp.value;
+  value = comp.value;
   let id = comp.id;
 
   if (
     id === "desnudaChocolate" ||
-    id == "desnudaCasera" ||
+    id === "desnudaCasera" ||
     id === "desnudaRed"
   ) {
     porciones.style.display = "block";
     document.getElementById(
       "productoSeleccionado"
     ).innerText = `Elegiste: ${value}.`;
+    boolean = true;
   } else {
     porciones.style.display = "none";
     document.getElementById(
@@ -52,7 +59,7 @@ const formulario = document.querySelector("#formWh");
 const buttonSubmit = document.querySelector("#submitForm");
 const urlDesktop = "https://web.whatsapp.com/";
 const urlMobile = "whatsapp://";
-const telefono = "3214544853";
+const telefono = "573214544853";
 
 formulario.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -62,11 +69,82 @@ formulario.addEventListener("submit", (e) => {
     let name = document.querySelector("#firstName").value;
     let torta = document.querySelector("#cantPorciones").value;
     let phone = document.querySelector("#phone").value;
-    let data = document.querySelector("#datepicker").value;
+    let data = document.querySelector("#date").value;
     let time = document.querySelector("#time").value;
     let nameR = document.querySelector("#nameRecibe").value;
     let phoneR = document.querySelector("#phoneRecibe").value;
-    let address = document.querySelector("#address");
+    let address = document.querySelector("#address").value;
     let msg = document.querySelector("#message").value;
+    let adiciones = document.querySelector("add").value;
+    //verificamos si es torta o no
+    if (boolean != true) {
+      console.log(boolean + " Sin porciones ");
+      let mensaje =
+        "send?phone=" +
+        telefono +
+        "&text=*Formulario de compra Rotri Reposteria*%0A*¿Qué producto deseas?*%0A" +
+        value +
+        "*%0A*¿Cuáles es tu nombre?*%0A" +
+        name +
+        "*%0A*¿Cuáles es tu número celular?*%0A" +
+        phone +
+        "*%0A*¿Fecha de entrega?*%0A" +
+        data +
+        "*%0A*¿Hora de entrega?*%0A" +
+        time +
+        "*%0A*¿Nombre de quien recibe?*%0A" +
+        nameR +
+        "*%0A*¿Celular de quien recibe?*%0A" +
+        phoneR +
+        "*%0A*¿Dirección de entrega?*%0A" +
+        address +
+        "*%0A*¿Mensaje para la tarjeta?*%0A" +
+        msg +
+        "*%0A*¿Alguna adición?*%0A" +
+        adiciones +
+        !"";
+      if (isMobile()) {
+        window.open(urlMobile + mensaje, "_blank");
+      } else {
+        window.open(urlDesktop + mensaje, "_blank");
+      }
+    } else {
+      console.log(boolean + "Con porciones");
+      if (torta == 1) {
+        console.log("Sin porciones" + data + time + "Porciones= " + "6");
+        console.log(
+          phone +
+            " " +
+            nameR +
+            " " +
+            phoneR +
+            " " +
+            address +
+            " " +
+            msg +
+            " " +
+            adiciones +
+            " " +
+            name
+        );
+      } else {
+        console.log("Sin porciones" + data + time + "Porciones= " + "15");
+        // console.log(
+        //   phone +
+        //     " " +
+        //     nameR +
+        //     " " +
+        //     phoneR +
+        //     " " +
+        //     address +
+        //     " " +
+        //     msg +
+        //     " " +
+        //     adiciones +
+        //     " " +
+        //     name
+        // );
+      }
+    }
   });
 });
